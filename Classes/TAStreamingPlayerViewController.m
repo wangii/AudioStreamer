59d9d873398b32841f8b75e74d16711abe5a857f
Walter Tyree <walter@tyreeapps.com>
2010-07-27 12:29:14 -0400

    Added 2x graphics for the iPhone 4. Took out the animation during the loading phase since it looked odd.

--- Classes/TAStreamingPlayerViewController.m
+++ Classes/TAStreamingPlayerViewController.m
@@ -64,9 +64,7 @@
 // and populate the rows and columns (components) with data.
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
 	
-	// we have to loop through the datasource and pull out the titles for our pickerdata....
-	NSLog(@"this is the row: %d for component %d", row, 1);
-	
+		
 	NSString *title    = [NSString stringWithFormat:@"%@", [[self.streams objectAtIndex:row] objectAtIndex:1]];
 	return title;
 	
@@ -98,10 +96,6 @@
 	{
 		[button setImage:image forState:0];
 		
-		if ([button.currentImage isEqual:[UIImage imageNamed:@"loadingbutton.png"]])
-		{
-			[self spinButton];
-		}
 	}
 }
 
@@ -199,53 +193,6 @@
 	[self setButtonImage:[UIImage imageNamed:@"playbutton.png"]];
 }
 
-//
-// spinButton
-//
-// Shows the spin button when the audio is loading. This is largely irrelevant
-// now that the audio is loaded from a local file.
-//
-- (void)spinButton
-{
-	[CATransaction begin];
-	[CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-	CGRect frame = [button frame];
-	button.layer.anchorPoint = CGPointMake(0.5, 0.5);
-	button.layer.position = CGPointMake(frame.origin.x + 0.5 * frame.size.width, frame.origin.y + 0.5 * frame.size.height);
-	[CATransaction commit];
-
-	[CATransaction begin];
-	[CATransaction setValue:(id)kCFBooleanFalse forKey:kCATransactionDisableActions];
-	[CATransaction setValue:[NSNumber numberWithFloat:2.0] forKey:kCATransactionAnimationDuration];
-
-	CABasicAnimation *animation;
-	animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-	animation.fromValue = [NSNumber numberWithFloat:0.0];
-	animation.toValue = [NSNumber numberWithFloat:2 * M_PI];
-	animation.timingFunction = [CAMediaTimingFunction functionWithName: kCAMediaTimingFunctionLinear];
-	animation.delegate = self;
-	[button.layer addAnimation:animation forKey:@"rotationAnimation"];
-
-	[CATransaction commit];
-}
-
-//
-// animationDidStop:finished:
-//
-// Restarts the spin animation on the button when it ends. Again, this is
-// largely irrelevant now that the audio is loaded from a local file.
-//
-// Parameters:
-//    theAnimation - the animation that rotated the button.
-//    finished - is the animation finised?
-//
-- (void)animationDidStop:(CAAnimation *)theAnimation finished:(BOOL)finished
-{
-	if (finished)
-	{
-		[self spinButton];
-	}
-}
 
 //
 // buttonPressed:
